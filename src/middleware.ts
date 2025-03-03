84514d86f13b41cd34abce89dc122bc505645e6f
princedev3 <marvinprince232@gmail.com>
2025-03-03 21:53:17 +0200

    new

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,14 +5,6 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
-  const path = req.nextUrl;
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.nextUrl));
-  }
-  if (token && token.role !== "ADMIN") {
-    return NextResponse.redirect(new URL("/", req.nextUrl));
-  }
   // if (session?.user && authRoute.includes(nextUrl.pathname)) {
   //   if (
   //     session.user?.customExpiration &&
